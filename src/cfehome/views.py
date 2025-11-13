c22cd9763bb6e5c66d8a2dfc06ceb8cfa0db3de1
aosum <abel.osumi@grispen.com>
2025-11-13 14:03:55 -0600

    First commit

---
+++ src/cfehome/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from visits.models import PageVisit
+
+
+def home_page_view(request, *args, **kwargs):
+    path = request.path
+    queryset = PageVisit.objects.all()
+    page_queryset = PageVisit.objects.filter(path=path)
+
+    html_template = "home.html"
+    my_title = "This is my title"
+
+
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count":page_queryset.count(),
+        "page_visit_percentage":page_queryset.count()/queryset.count() * 100,
+        "total_visit_count":queryset.count(),
+    }
+    PageVisit.objects.create(path=path)
+    return render(request, html_template, my_context)
