f374ca02fa89d8f7fc268a3932268cb34f9a2e90
aosum <abel.osumi@grispen.com>
2025-12-09 17:00:17 -0600

    Add basic auth app with login and registration views

---
+++ src/auth/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username') or None
+        password = request.POST.get('password') or None
+        if all([username, password]):
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+    return render(request, 'auth/login.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username') or None
+        email = request.POST.get('email') or None
+        password = request.POST.get('password') or None
+        # Django forms will handle validation
+        # user_exists = User.objects.filter(username__iexact=username).exists()
+        # email_exists = User.objects.filter(email__iexact=email).exists()
+        User.objects.create_user(username=username, email=email, password=password)
+        return redirect('/login')
+    return render(request, 'auth/register.html')
