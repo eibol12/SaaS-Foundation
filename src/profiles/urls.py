d49bbece35438d5df309ee38d03260dbcacba628
aosum <abel.osumi@grispen.com>
2025-12-16 16:53:43 -0600

    Add profile detail and list views with templates and URL updates
    - Added `profile_detail_view` and `profile_list_view` to display user profiles and active users respectively.
    - Created `detail.html` and `list.html` templates for profile-related views.
    - Updated URLs to include routes for profile detail and list views.

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("<str:username>", views.profile_view, name="profile"),
+    path("<str:username>", views.profile_detail_view, name="profile"),
+    path("", views.profile_list_view, name="list"),
 ]
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+from visits.models import PageVisit
+
 
 User = get_user_model()
 
+@login_required
+def profile_list_view(request):
+    context = {
+        "object_list": User.objects.filter(is_active = True)
+    }
+    return render(request, "profiles/list.html", context)
+
+
+
 # Create your views here.
 @login_required
-def profile_view(request, username = None ,*args, **kwargs):
-    # profile_user_obj = User.objects.get(username=username)
+def profile_detail_view(request, username = None ,*args, **kwargs):
+    user = request.user
     profile_user_obj = get_object_or_404(User, username=username)
-    return HttpResponse(f"Welcome to your profile {profile_user_obj.username}.")
+    is_me = profile_user_obj == user
+    context = {
+        "object": profile_user_obj,
+        "instance": profile_user_obj,
+        "owner": is_me,
+    }
+    return render(request, "profiles/detail.html", context)
